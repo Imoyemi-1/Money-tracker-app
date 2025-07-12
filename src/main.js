532ec21f29b2058da613f568fb68ef6e8be200c4
Imoyemi <okeibrahim133@gmail.com>
2025-07-12 09:24:50 -0500

    Feat: handle dropdown display

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import './style.css';
+
+//
+
+const setupMainEl = document.getElementById('setup-main');
+const dropDownEl = document.querySelectorAll('.dropdown');
+
+// display click dropdown
+
+const handleDropDown = (e) => {
+  const dropDownWrapper = e.target.closest('.items-container');
+
+  if (dropDownWrapper) {
+    e.stopPropagation();
+    const parent = dropDownWrapper.parentElement;
+    dropDownEl.forEach((el) => el.classList.remove('show'));
+    parent.querySelector('.dropdown').classList.add('show');
+  }
+};
+
+// Eventlistener
+setupMainEl.addEventListener('click', handleDropDown);
+document.addEventListener('click', () => {
+  dropDownEl.forEach((el) => el.classList.remove('show'));
+});
