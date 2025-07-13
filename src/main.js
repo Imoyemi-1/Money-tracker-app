b0c4f0dd29033c8e0a128b0b3e9e4ccccec587ae
Imoyemi <okeibrahim133@gmail.com>
2025-07-13 04:42:58 -0500

    feat: handle selected dropdown item
    for user to be able  dropdown item in individual dropdown

--- src/main.js
+++ src/main.js
@@ -41,31 +41,65 @@ const displayBaseCurrencies = async () => {
 
 // display click dropdown
 
-const handleDropDown = (e) => {
+// open and close of dropdown if the input is click
+const toggleDropDown = (e) => {
   const dropDownWrapper = e.target.closest('.items-container');
-
+  const selectedEl = document.querySelectorAll('.selected');
   if (dropDownWrapper) {
+    const dropDownInput = dropDownWrapper.querySelector('input');
+    const dropDownItem = dropDownWrapper.querySelector('.selected');
+
     e.stopPropagation();
     const parent = dropDownWrapper.parentElement;
     dropDownEl.forEach((el) => {
       el.classList.remove('show');
       el.classList.remove('border');
     });
+
     itemContainerEl.forEach((item) => item.classList.remove('open-border'));
+    selectedEl.forEach((item) => item.classList.remove('blur'));
+
+    if (dropDownInput) {
+      dropDownInput.focus();
+      dropDownItem.classList.add('blur');
+    }
+
     parent.querySelector('.dropdown').classList.add('show');
     dropDownWrapper.classList.add('open-border');
     parent.querySelector('.dropdown').classList.add('border');
   }
 };
 
+// select base currency from dropdown
+
+const selectListItem = (e) => {
+  const dropDownItemEl = e.target.closest('.dropdown-item');
+
+  if (dropDownItemEl) {
+    const dropDownItemParentElID = dropDownItemEl.parentElement.id;
+    if (dropDownItemParentElID === 'base-dropdown') console.log('base');
+    else if (dropDownItemParentElID === 'group-dropdown') console.log('group');
+    else console.log('add');
+  }
+};
+
+// handle what handle dropdown and if its click
+
+const handleDropDown = (e) => {
+  toggleDropDown(e);
+  selectListItem(e);
+};
+
 // Eventlistener
 setupMainEl.addEventListener('click', handleDropDown);
 document.addEventListener('click', () => {
+  const selectedEl = document.querySelectorAll('.selected');
   dropDownEl.forEach((el) => {
     el.classList.remove('show');
     el.classList.remove('border');
   });
 
   itemContainerEl.forEach((item) => item.classList.remove('open-border'));
+  selectedEl.forEach((item) => item.classList.remove('blur'));
 });
 displayBaseCurrencies();
