6a6129108a98332af4aa524da3d103caf169394a
Imoyemi <okeibrahim133@gmail.com>
2025-07-30 20:24:09 -0500

    feat: add income logic

--- src/Tracker.js
+++ src/Tracker.js
@@ -36,7 +36,7 @@ class Tracker {
       .flat()
       .find((acc) => acc.id === accountId);
     if (curCode === account.baseCurrency.currencyName) {
-      account.baseCurrency.amount = account.baseCurrency.amount - amount;
+      account.baseCurrency.amount -= amount;
     } else {
       const additional = account.additionalCurrencies.find(
         (acc) => acc.code === curCode
@@ -44,8 +44,31 @@ class Tracker {
       additional.amount -= amount;
     }
   }
+
+  addIncome({ accountId, curCode, amount }) {
+    const account = Object.values(this.accounts)
+      .flat()
+      .find((acc) => acc.id === accountId);
+    if (curCode === account.baseCurrency.currencyName) {
+      account.baseCurrency.amount += amount;
+    } else {
+      const additional = account.additionalCurrencies.find(
+        (acc) => acc.code === curCode
+      );
+      additional.amount += amount;
+    }
+  }
+
+  addTransfer({ toaccountId, curCode, amount, form }) {}
+
+  getAccounts() {
+    console.log(Object.values(this.accounts)[0][0]);
+  }
 }
 
 const moneyTracker = new Tracker();
 
 moneyTracker.addExpense({ accountId: '08993', curCode: 'USD', amount: 100 });
+moneyTracker.getAccounts();
+moneyTracker.addIncome({ accountId: '08993', curCode: 'ALL', amount: 100 });
+moneyTracker.getAccounts();
